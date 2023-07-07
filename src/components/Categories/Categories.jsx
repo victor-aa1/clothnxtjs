edb50de02eb6083accfde6477921970a289641ca
victor.agbeo <130013310+victor-aa1@users.noreply.github.com>
2023-07-07 02:46:14 +0000

    m

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,12 @@
+import './style/categories.styles.scss'
+import React from 'react'
+
+const CategoriesItem = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CategoriesItem
